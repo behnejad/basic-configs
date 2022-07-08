5d7d29f38a81fa2fab2376b067ddf89295a0430e
Behnejad <3690943+behnejad@users.noreply.github.com>
2022-07-08 19:39:47 +0430

    Create windows.cmd

---
+++ windows.cmd
@@ -0,0 +1,4 @@
+Dism.exe /Online /Cleanup-Image /StartComponentCleanup
+Dism.exe /Online /Cleanup-Image /AnalyzeComponentStore analyzes
+Dism.exe /Online /Cleanup-Image /ScanHealth
+Dism.exe /Online /Cleanup-Image /RestoreHealth 
