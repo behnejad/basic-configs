56145803fd5d0468b20b03b698db203dd211bac6
Behnejad <3690943+behnejad@users.noreply.github.com>
2022-09-17 18:39:05 +0430

    Update windows.cmd

--- windows.cmd
+++ windows.cmd
@@ -2,3 +2,4 @@ Dism.exe /Online /Cleanup-Image /StartComponentCleanup
 Dism.exe /Online /Cleanup-Image /AnalyzeComponentStore analyzes
 Dism.exe /Online /Cleanup-Image /ScanHealth
 Dism.exe /Online /Cleanup-Image /RestoreHealth 
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize 
