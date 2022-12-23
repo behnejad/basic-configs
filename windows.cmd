24dfe42ae28a57ace5929340b1f783116e1b9c54
Behnejad <3690943+behnejad@users.noreply.github.com>
2022-12-23 19:43:05 +0330

    Update windows.cmd

--- windows.cmd
+++ windows.cmd
@@ -6,4 +6,6 @@ Dism.exe /Online /Disable-Feature:Microsoft-Hyper-V
 
 bcdedit /set hypervisorlaunchtype off
 
+powercfg /hibernate /size 10
+
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize 
