e15c5a91e48f1666f3a2ca623dd9e04fd9b78f97
Behnejad <3690943+behnejad@users.noreply.github.com>
2023-01-09 19:22:35 +0330

    Update qemu.sh

--- virtualization/qemu.sh
+++ virtualization/qemu.sh
@@ -1,2 +1,12 @@
 qemu-system-x86_64 vm.qcow2 -netdev tap,id=mynet0,ifname=tap -device e1000,netdev=mynet0
 qemu-img convert -f vdi test.vdi -O qcow2 test.qcow2
+
+# mount qcow2
+modprobe nbd max_part=8
+qemu-nbd --connect=/dev/nbd0 1.qcow2
+fdisk /dev/nbd0 -l
+mount /dev/nbd0p1 /mnt/somepoint/
+umount /mnt/somepoint/
+qemu-nbd --disconnect /dev/nbd0
+rmmod nbd
+# end mount
