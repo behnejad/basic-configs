7d4ea3cc11313d22606bc3e67f67366ba1a22499
Behnejad <3690943+behnejad@users.noreply.github.com>
2023-02-08 20:06:41 +0330

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,6 +1,6 @@
 # mount cifs
 sudo mount -t cifs -o user=user,password=pwd,uid=$(id -u),gid=$(id -g) //192.168.1.1/dir /mnt'
-
+sshfs a@a:/home/a/ /media/remote/
 
 #journalctl clean
 journalctl --vacuum-size=500M
