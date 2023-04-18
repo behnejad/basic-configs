8468424aaeb066f631ce98d99eaeac15e6164361
Behnejad <3690943+behnejad@users.noreply.github.com>
2023-04-18 15:26:54 +0330

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,6 +1,6 @@
 # install essentials
 sudo apt update
-sudo apt install aria2 htop bmon iotop nginx tmux net-tools ncdu
+sudo apt install vim aria2 htop bmon iotop nginx tmux net-tools ncdu build-essential openssh-server gparted 
 
 # mount cifs
 sudo mount -t cifs -o user=xxx,password=xxx,uid=$(id -u),gid=$(id -g) //192.168.1.1/dir /mnt'
@@ -8,6 +8,9 @@ sshfs a@a:/home/a/ /media/remote/
 
 #journalctl clean
 journalctl --vacuum-size=500M
+[Journal]
+SystemMaxUse=250M
+SystemMaxFileSize=50M
 
 # clean caches
 echo 3 | sudo tee /proc/sys/vm/drop_caches
