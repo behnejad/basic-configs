c7a9ea1a3dcdda4a7610be4d3ab926f0769323b8
Behnejad <3690943+behnejad@users.noreply.github.com>
2023-05-08 07:38:56 +0330

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,6 +1,10 @@
 # install essentials
 sudo apt update
-sudo apt install vim aria2 htop bmon iotop nginx tmux net-tools ncdu build-essential openssh-server gparted 
+sudo apt install -y vim aria2 htop bmon iotop nginx tmux net-tools ncdu build-essential openssh-server gparted
+sudo apt install -y qemu-system libvirt0 virt-manager guestfs-tools
+sudo apt install -y openjdk-8-jdk openjdk-11-jdk openjdk-19-jdk
+
+# install wine, sublime, virtualbox, veracrypt
 
 # mount cifs
 sudo mount -t cifs -o user=xxx,password=xxx,uid=$(id -u),gid=$(id -g) //192.168.1.1/dir /mnt'
