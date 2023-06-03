8e651f31dc0ee9d68fc7ddfca5241c99e1db0be9
Behnejad <3690943+behnejad@users.noreply.github.com>
2023-06-03 08:10:51 +0330

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,8 +1,11 @@
 # install essentials
 sudo apt update
-sudo apt install -y vim aria2 htop bmon iotop nginx tmux net-tools ncdu build-essential openssh-server gparted
+sudo apt install -y vim aria2 htop bmon iotop nginx tmux net-tools ethtool ncdu build-essential openssh-server gparted
 sudo apt install -y qemu-system libvirt0 virt-manager guestfs-tools cifs-utils
 sudo apt install -y openjdk-8-jdk openjdk-11-jdk openjdk-19-jdk
+sudo apt install -y firejail notepadqq
+
+firejail --dns=1.1.1.1 --net=wlx0024a59c6ea5 --defaultgw=192.168.160.1 aria2c "google.com"
 
 # install wine, sublime, virtualbox, veracrypt
 
