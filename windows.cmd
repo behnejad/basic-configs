c73657814e4c286ac32406396aed4f3e0c4fe406
Behnejad <3690943+behnejad@users.noreply.github.com>
2024-05-10 16:42:50 +0330

    Update windows.cmd

--- windows.cmd
+++ windows.cmd
@@ -3,6 +3,8 @@ Dism.exe /Online /Cleanup-Image /AnalyzeComponentStore analyzes
 Dism.exe /Online /Cleanup-Image /ScanHealth
 Dism.exe /Online /Cleanup-Image /RestoreHealth 
 Dism.exe /Online /Disable-Feature:Microsoft-Hyper-V
+Dism.exe /Online /Set-ReservedStorageState /State:Disabled
+Dism.exe /Online /Get-ReservedStorageState
 
 bcdedit /set hypervisorlaunchtype off
 
@@ -16,3 +18,5 @@ setup.exe --uninstall --system-level --verbose-logging --force-uninstall
 
 services -> Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\
 defender -> HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender DisableAntiSpyware
+
+Godmode -> new folder name -> GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}
