e816e17a679859edd4641124f03717f822f753b3
Behnejad <3690943+behnejad@users.noreply.github.com>
2025-04-11 07:14:39 +0000

    Create git.sh

---
+++ git.sh
@@ -0,0 +1,14 @@
+# ~/.ssh/config
+StrictHostKeyChecking no
+HOST	gitlab.com
+	    HostName gitlab.com
+    	IdentityFile /h/id_rsa
+	    User git
+      Port 22
+      PubkeyAuthentication yes
+
+
+ssh-agent $(ssh-add /media/id_rsa; git pull --all)
+
+git config core.autocrlf true/false
+git config --global core.fileMode false
